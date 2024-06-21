fa4bb2713358408271694c799d3d46d4df1ce594
Patrick Xiong <PXstiegler@gmail.com>
2024-06-21 16:32:56 -0400

    1st commit

---
+++ debugging/debug.js
@@ -0,0 +1,52 @@
+const storeOwners = [
+    {
+      name: 'charles',
+      stores: 1,
+      location: "AZ",
+    },
+    {
+      name: 'sally',
+      stores: 1,
+      location: "NM",
+    },
+    {
+      name: 'cassandra',
+      stores: 1,
+      location: "NM",
+    },
+    {
+      name: 'Danny Shavez',
+      stores: 1,
+      location: "NM",
+    },
+  ];
+  
+  const listNumberOfStores = function () {
+    let totalLocations = 0;
+    for (let i = 0; i < storeOwners.length; i++) {
+      totalLocations += storeOwners[i].stores;
+    }   
+    return totalLocations;
+  };
+  
+  let locations = listNumberOfStores();
+  
+  function tellMeMyStores() {
+    console.log('Hey, can you tell me who owns stores?');
+    }
+  
+  function hasStore() {
+    for (let i = 0; i < storeOwners.length; i++) {
+      let person = storeOwners[i].name;
+      let location = storeOwners[i].location;
+      if (person === 'Danny Shavez') {
+        console.log(`Yes, ${person} that lives in ${location} owns one too.`);
+      } else {
+        console.log(`Yes, ${person} has one`);
+      }
+    }
+  }
+  
+  tellMeMyStores();
+  hasStore();
+  
